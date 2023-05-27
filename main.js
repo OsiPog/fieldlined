8697df6659a2a94d9f2731628a2064f2fe9327e5
OsiPog <osibluber@protonmail.com>
2023-05-27 13:10:10 +0200

    inital commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// const canvas = document.querySelector("canvas")
+// const gl = canvas.getContext('webgl', { antialias: true })
+// const rust = await import('./pkg/fieldlined')
+
+// if (!gl) {
+//     throw "Failed to initialize WebGL"
+// }
+
+// const FRAME_TIME = 1000 / 60;
+
+// const client = new rust.Client()
+
+// client.say("Hello")
+console.log("Hello")
